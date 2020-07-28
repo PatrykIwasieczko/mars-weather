26d4efad5febd78f1498fd80316a650df05d168c
Patryk <patryk.iwasieczko@gmail.com>
2020-07-28 16:00:43 +0200

    Fetch and display data from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // React
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // Components
 import WeatherData from "./components/WeatherData";
@@ -7,8 +7,46 @@ import WeatherData from "./components/WeatherData";
 // Styles
 import "./App.css";
 
+// Utils
+import axios from "axios";
+import { API_URL } from "./API";
+
 const App = () => {
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [weather, setWeather] = useState([]);
+    const [selectedDay, setSelectedDay] = useState();
+
+    useEffect(() => {
+        getWeatherData();
+    }, []);
+
+    const getWeatherData = async () => {
+        const data = await axios.get(API_URL);
+        const marsWeather = data.data.sol_keys.map((key) => {
+            return {
+                dayKey: key,
+                maxTemp: data.data[key].AT?.mx || "No data",
+                minTemp: data.data[key].AT?.mn || "No data",
+                pressure: data.data[key].PRE?.av,
+                windSpeed: Math.round(data.data[key].HWS?.av || 0),
+                windDirectionDegrees:
+                    data.data[key].WD?.most_common?.compass_degrees,
+                date: new Date(data.data[key].First_UTC).toLocaleDateString(
+                    undefined,
+                    {
+                        day: "numeric",
+                        month: "long",
+                    }
+                ),
+            };
+        });
+        setWeather(marsWeather);
+        setSelectedDay(marsWeather.length - 1);
+        setLoading(false);
+
+        console.log(marsWeather);
+    };
+
     return (
         <div className="App">
             <div className="wrapper">
@@ -18,7 +56,7 @@ const App = () => {
                     ) : (
                         <>
                             <h1>Latest weather at Elysium Plantitia</h1>
-                            <WeatherData />
+                            <WeatherData data={weather[selectedDay]} />
                         </>
                     )}
                 </div>
--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -4,31 +4,46 @@ import React from "react";
 // Styles
 import "./WeatherData.css";
 
-const WeatherData = () => {
+const WeatherData = ({ data }) => {
+    const {
+        dayKey,
+        date,
+        minTemp,
+        maxTemp,
+        pressure,
+        windSpeed,
+        windDirectionDegrees,
+    } = data;
     return (
         <div className="weather-wrapper">
             <div className="date">
-                <h2>Day</h2>
-                <p>Date</p>
+                <h2>{dayKey}</h2>
+                <p>{date}</p>
             </div>
             <div className="temperature">
                 <h2 className="title">Temperature</h2>
                 <p className="reading">
                     Highest:
-                    <span>MaxTemp</span>
+                    <span>{maxTemp}</span>
                     <span>C or F</span>
                 </p>
                 <p className="reading">
                     Lowest:
-                    <span>MinTemp</span>
+                    <span>{minTemp}</span>
                     <span>C or F</span>
+                </p>
+            </div>
+            <div>
+                <h2>Pressure</h2>
+                <p>
+                    <span>{pressure} Pa</span>
                 </p>
             </div>
             <div>
                 <h2>Wind</h2>
                 <p>
-                    <span>Wind direction</span>
-                    <span> kph or mph</span>
+                    <span>{windDirectionDegrees}</span>
+                    <span>{windSpeed} kph or mph</span>
                 </p>
             </div>
         </div>
