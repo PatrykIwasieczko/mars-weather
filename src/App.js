d6d6467e77e9dc3b35f82f73a8820daae16a2a3c
Patryk <patryk.iwasieczko@gmail.com>
2020-07-28 15:09:32 +0200

    Set up bg image and basic styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h1>Mars Weather app</h1>
+            <div className="wrapper">
+                <h1>Mars weather</h1>
+            </div>
         </div>
     );
 }
